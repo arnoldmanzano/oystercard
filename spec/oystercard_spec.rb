c11b945336e5ddeb33d08901d17a32bc108ea7c8
Marion LV <marion.levisage@gmail.com>
2016-01-27 11:57:05 +0000

    Refactored challenge 14

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -26,45 +26,52 @@ describe Oystercard do
     end
   end
 
-  describe '#touch_in(station)' do
+  describe '#inferior_balance' do
     it 'raises an exception if the balance is inferior to £1' do
       expect{ oystercard.touch_in(entry_station) }.to raise_error 'Please top up your card.'
     end
+  end
+
+  describe '#touch_in(station)' do
+    before(:each) do
+      oystercard.top_up(10)
+    end
 
     it 'starts journey when card is touched in (Manzano style)' do
-      oystercard.top_up(10)
       expect(oystercard.journey).to receive(:start)
       oystercard.touch_in(entry_station)
     end
 
     it 'is in in_journey when the card is touched in' do
-      oystercard.top_up(10)
       oystercard.touch_in(entry_station)
       expect(oystercard.journey).to be_in_journey
     end
   end
 
   describe '#touch_out' do
+
+    before(:each) do
+      oystercard.top_up(10)
+      oystercard.touch_in(entry_station)
+    end
+
     it 'reduces the balance by the minimum fare' do
       expect{oystercard.touch_out(exit_station)}.to change { oystercard.balance }.by -Oystercard::FARE_MIN
     end
 
     it 'ends journey when card is touched out (Manzano style)' do
-      oystercard.top_up(10)
       expect(oystercard.journey).to receive(:end)
       oystercard.touch_out(exit_station)
     end
 
     it 'is in not in_journey when the card is touched out' do
-      oystercard.top_up(10)
-      oystercard.touch_in(entry_station)
       oystercard.touch_out(exit_station)
       expect(oystercard.journey).to be_complete
     end
   end
 
   describe '#fare' do
-    xit 'returns the minimum fare when a journey is complete' do
+    it 'returns the minimum fare when a journey is complete' do
       oystercard.top_up(10)
       oystercard.touch_in(entry_station)
       oystercard.touch_out(exit_station)
